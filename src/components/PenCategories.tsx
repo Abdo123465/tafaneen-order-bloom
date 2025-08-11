d060bffb7e1abe7e1087d3397673d2054a6fde01
Abdo123465 <tafaneensps@gmail.com>
2025-08-11 22:31:40 +0300

    Update PenCategories.tsx

--- src/components/PenCategories.tsx
+++ src/components/PenCategories.tsx
@@ -1,4 +1,3 @@
-
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
@@ -145,3 +144,8 @@ export function PenCategories() {
               </Card>
             );
           })}
+        </div>
+      </div>
+    </section>
+  );
+}
