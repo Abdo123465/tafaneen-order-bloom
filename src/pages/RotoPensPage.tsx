b7b58db407611e0958140ba22f42703299f9df03
Abdo123465 <tafaneensps@gmail.com>
2025-08-14 20:52:31 +0300

    Update RotoPensPage.tsx

--- src/pages/RotoPensPage.tsx
+++ src/pages/RotoPensPage.tsx
@@ -51,7 +51,7 @@ const rotoPens = [
   { id: 'Roto-Glase-red', name: 'قلم روتو جلاس احمر', price: 7, image: RotoGlaseredImage, description: 'قلم حبر سائل 0.7 مم لكتابة ناعمة ودقيقة' },
   { id: 'Roto-Glase-black', name: 'قلم روتو جلاس اسود', price: 7, image: RotoGlaseblackImage, description: 'قلم حبر سائل 0.7 مم لكتابة ناعمة ودقيقة' },
   { id: 'Roto-Pyramid-blue', name: 'قلم روتو بيراميد ازرق', price: 7, image: RotoPyramidblueImage, description: 'قلم حبر سائل 0.7 مم لكتابة ناعمة ودقيقة' },
-  { id: 'ROTO-RACER-blue', name: 'قلم روتو بيراميد ازرق', price: 7, image: ROTORACERblueImage, description: 'قلم حبر سائل 0.7 مم لكتابة ناعمة ودقيقة' },
+  { id: 'ROTO-RACER-blue', name: 'قلم روتو راسير ازرق', price: 7, image: ROTORACERblueImage, description: 'قلم حبر سائل 0.7 مم لكتابة ناعمة ودقيقة' },
 
 
 ];
